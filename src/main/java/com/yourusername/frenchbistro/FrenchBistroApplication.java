11db4743e49ed449fa95151655133b219bc676eb
jocomst <johncomstock9716@gmail.com>
2023-12-27 13:53:17 -0500

    adding main function

---
+++ src/main/java/com/yourusername/frenchbistro/FrenchBistroApplication.java
@@ -0,0 +1,15 @@
+package com.yourusername.frenchbistro;
+
+public class FrenchBistroApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to French Bistro!");
+        // Initialize your application here
+
+        // Example: Start a server, initialize a database connection, etc.
+
+        // Keep the application running until it's stopped
+        // For web applications, this is usually handled by the server framework
+    }
+}
+
